b11632c382651abdf2dd14aace60afcb164efa98
manxyrisen <bmanxy@gmail.com>
2018-05-01 15:20:27 -0500

    afternoon2 part2

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr){
+  return arr[0];
+}
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr[arr.length-1];
+}
 
 
 
@@ -47,10 +51,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
-
-
-
+function looper(family){
+  for(i=0;i<family.length;i++){
+    alert(family[i])
+  }
+}
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -62,7 +67,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop through the given array backwards alerting every item in the array starting at the end.\
 */
 
-//Code Here
+function reversedLooper(letters){
+  for(i=letters.length-1;i>=0;i--){
+    alert(letters[i])
+  }
+}
 
 
 
@@ -77,7 +86,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums){
+  var evensArr = [];
+  for(i=0;i<nums.length;i++){
+    if(nums[i]%2==0){
+       evensArr.push(nums[i])
+    }
+  } return evensArr;
+}
 
 
 
@@ -94,7 +110,15 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
   Have divider return an Array with the first item in the array being the evens array (all the even values from numbersArray) and the second item in the Array being the odds array (all the odd values from numbersArray).
 */
 
-//Code Here
+function divider(numbersArray){
+  var evensArray = [];
+  var oddsArray = [];
+  for(i=0;i<numbersArray.length;i++){
+    if(numbersArray[i]%2==0){
+      evensArray.push(numbersArray[i])
+    } else oddsArray.push(numbersArray[i])
+  } return [evensArray,oddsArray];
+}
 
 
 
@@ -115,7 +139,16 @@ var getRandomArbitrary = function() {
   If it is, return true, if it's not, return false
 */
 
-//Code Here
+function finder(arr){
+  var random =getRandomArbitrary();
+  console.log(random);
+  
+  for(i=0;i<arr.length;i++){
+    if(arr[i]===random){
+      return true;
+    } 
+  } return false;
+}
 
 
 
